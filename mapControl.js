7f6cf0318eafecd1c259a892650088d31a778d5a
Johanna Simfors <jsimfors@gmail.com>
2020-03-24 10:25:10 +0100

    loadscreen added

--- mapControl.js
+++ mapControl.js
@@ -44,6 +44,8 @@ window.addEventListener('beforeinstallprompt', (e) => {
 });
 
 
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 59.3498092, lng: 18.0684758},
